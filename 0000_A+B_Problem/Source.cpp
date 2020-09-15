31a521597c3a805fe618b61e8e8c24b71276a4ef
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-09-16 01:07:01 +0700

    master

--- 0000_A+B_Problem/Source.cpp
+++ 0000_A+B_Problem/Source.cpp
@@ -6,13 +6,12 @@ int main()
 {	
 	struct ans
 	{
-		int num[2], sum = 0;
+		int num[2] , sum = 0;
 	}avg;
 	
 	for (int i = 0; i < 2; i++) {
 
 		scanf_s("%d", &avg.num[i]);
-
 	}
 	avg.sum = positive(avg.num[0], avg.num[1]);
 	printf("%d", avg.sum);
